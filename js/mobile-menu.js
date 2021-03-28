ae4d1e46745000a72abce992f89dbb84e06feb60
malatkO <73491915+malatkO@users.noreply.github.com>
2021-03-28 22:36:14 +0300

    Update main.css

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,29 +1,29 @@
-( ( )  =>  {
-    const  mobileMenu  =  документ . querySelector ( '.js-меню-контейнер' ) ;
-    const  openMenuBtn  =  документ . querySelector ( '.js-open-menu' ) ;
-    const  closeMenuBtn  =  документ . querySelector ( '.js-close-menu' ) ;
-  
-    const  toggleMenu  =  ( )  =>  {
-      const  isMenuOpen  =
-        openMenuBtn . getAttribute ( 'aria- extended ' )  ===  'true'  ||  ложь ;
-      openMenuBtn . SetAttribute ( 'ария вспененные' ,  ! isMenuOpen ) ;
-      mobileMenu . classList . переключить ( 'открыто' ) ;
-  
-      const  scrollLockMethod  =  ! isMenuOpen
-        ? 'disableBodyScroll'
-        : 'enableBodyScroll' ;
-      bodyScrollLock [ scrollLockMethod ] ( документ . тело ) ;
-    } ;
-  
-    openMenuBtn . addEventListener ( «щелчок» ,  toggleMenu ) ;
-    closeMenuBtn . addEventListener ( «щелчок» ,  toggleMenu ) ;
-  
-    // Закрываем мобильное меню на более широких экранах
-    // в случае изменения ориентации устройства.
-    окно . matchMedia ( '(минимальная ширина: 768 пикселей)' ) . addEventListener ( 'изменить' ,  e  =>  {
-      если  ( ! e . соответствует )  return ;
-      mobileMenu . classList . удалить ( 'открыто' ) ;
-      openMenuBtn . setAttribute ( 'расширенная ария' ,  ложь ) ;
-      bodyScrollLock . enableBodyScroll ( документ . тело ) ;
-    } ) ;
-  } ) ( ) ;
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
